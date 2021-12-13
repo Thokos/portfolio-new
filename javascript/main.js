c9fa76f349e719e17061fe7506aca14805f4c718
Thokos <32060384+Thokos@users.noreply.github.com>
2021-12-13 17:35:55 +0100

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,88 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".bottom").forEach(element => {
+gsap.to(element, { 
+    trigger:element,
+    duration:0,
+    y:200,
+    opacity:0
+    });
+});
+
+
+gsap.utils.toArray(".bottom").forEach(element => {
+gsap.to(element, {
+    scrollTrigger: {
+        trigger: element,
+        start: "top 100%",
+        end: "bottom top",
+        toggleActions: "restart"
+    },
+    y:0,
+    duration:1,
+    opacity:1
+    });
+});
+
+gsap.utils.toArray(".left").forEach(element => {
+    gsap.to(element, { 
+        trigger:element,
+        duration:0,
+        x:-100,
+        opacity:0
+        });
+    });
+    
+    
+    gsap.utils.toArray(".left").forEach(element => {
+    gsap.to(element, {
+        scrollTrigger: {
+            trigger: element,
+            start: "top 100%",
+            end: "bottom top",
+            toggleActions: "restart"
+        },
+        x:0,
+        duration:1,
+        opacity:1
+        });
+    });
+
+    
+gsap.utils.toArray(".right").forEach(element => {
+    gsap.to(element, { 
+        trigger:element,
+        duration:0,
+        x:100,
+        opacity:0
+        });
+        });
+        
+        
+gsap.utils.toArray(".right").forEach(element => {
+    gsap.to(element, {
+            scrollTrigger: {
+                trigger: element,
+                start: "top 100%",
+                end: "bottom top",
+                toggleActions: "restart"
+            },
+            x:0,
+            duration:1,
+            opacity:1
+            });
+        });
+        
+        gsap.utils.toArray(".fade").forEach(element => {
+            gsap.to(element, {
+                    scrollTrigger: {
+                        trigger: element,
+                        start: "top 100%",
+                        end: "bottom top",
+                        toggleActions: "restart"
+                    },
+                    duration:1,
+                    opacity:1
+                    });
+                });
+                
