7972e3c43085113f9b48b8577bc90afb45732a1d
Thokos <32060384+Thokos@users.noreply.github.com>
2021-12-14 13:11:04 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -85,4 +85,114 @@ gsap.to(element, {
     });
 });
 
+gsap.utils.toArray(".footeritem1").forEach(element => {
+    gsap.to(element, { 
+        trigger:element,
+        duration:0,
+        y:50,
+        opacity:0,
+        x:-20
+        });
+    });
+    
+    
+    gsap.utils.toArray(".footeritem1").forEach(element => {
+    gsap.to(element, {
+        scrollTrigger: {
+            trigger: element,
+            start: "top 100%",
+            end: "bottom top",
+            toggleActions: "restart",
+           
+        },
+        x:0,
+        y:0,
+        duration:1,
+        opacity:1
+        });
+    });
+
+    gsap.utils.toArray(".footeritem2").forEach(element => {
+        gsap.to(element, { 
+            trigger:element,
+            duration:0,
+            y:50,
+            opacity:0,
+            x:-20
+            });
+        });
+        
+        
+        gsap.utils.toArray(".footeritem2").forEach(element => {
+        gsap.to(element, {
+            scrollTrigger: {
+                trigger: element,
+                start: "top 100%",
+                end: "bottom top",
+                toggleActions: "restart",
+               
+            },
+            x:0,
+            y:0,
+            delay: 0.1,
+            duration:1,
+            opacity:1
+            });
+        });
+
+        gsap.utils.toArray(".footeritem3").forEach(element => {
+            gsap.to(element, { 
+                trigger:element,
+                duration:0,
+                y:50,
+                opacity:0,
+                x:-20
+                });
+            });
+            
+            
+            gsap.utils.toArray(".footeritem3").forEach(element => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,
+                    start: "top 100%",
+                    end: "bottom top",
+                    toggleActions: "restart",
+                   
+                },
+                x:0,
+                y:0,
+                delay: 0.2,
+                duration:1,
+                opacity:1
+                });
+            });
+
+
+gsap.utils.toArray(".footeritem4").forEach(element => {
+    gsap.to(element, { 
+        trigger:element,
+        duration:0,
+        y:50,
+        opacity:0,
+        x:-20
+        });
+    });
+                
+                
+gsap.utils.toArray(".footeritem4").forEach(element => {
+gsap.to(element, {
+    scrollTrigger: {
+    trigger: element,
+    start: "top 100%",
+    end: "bottom top",
+    toggleActions: "restart",   
+    },
+    x:0,
+    y:0,
+    delay: 0.3,
+    duration:1,
+    opacity:1
+    });
+});
 
